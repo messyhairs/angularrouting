143588476423a2e988a38abeed2491c9bacf1583
gokulnath <gokul@getpitstop.com>
2020-03-12 18:33:37 +0530

    filter search select-down

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { DeviceDetectorModule } from 'ngx-device-detector';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
@@ -11,6 +12,7 @@ import { WedoComponent } from './wedo/wedo.component';
 import { WehaveComponent } from './wehave/wehave.component';
 import { HotelComponent } from './hotel/hotel.component';
 import { JourneyComponent } from './journey/journey.component';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
@@ -22,12 +24,14 @@ import { JourneyComponent } from './journey/journey.component';
     WedoComponent,
     WehaveComponent,
     HotelComponent,
-    JourneyComponent
+    JourneyComponent,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DeviceDetectorModule.forRoot()
+    DeviceDetectorModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/wehave/wehave.component.ts
+++ src/app/wehave/wehave.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, VERSION, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Pipe, PipeTransform } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FilterPipe } from './filter.pipe';
 import { Routes, RouterModule, ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -9,11 +13,88 @@ import { Routes, RouterModule, ActivatedRoute, Router } from '@angular/router';
 export class WehaveComponent implements OnInit {
   snapshotparam: string;
   subscribedparam: string;
+  selectedgames: { name: string; id: number; selected: boolean; }[];
+  name: string;
+  searchText: string;
+  selectedcount = 0;
+
+
+  // Data Object to List Games
+  games = [
+    {
+      name: 'Chess',
+      id: 1,
+      selected: true
+    },
+    {
+      name: 'Ludo',
+      id: 2,
+      selected: false
+    },
+    {
+      name: 'Snakes & Ladders',
+      id: 3,
+      selected: false
+    },
+    {
+      name: 'Carrom',
+      id: 4,
+      selected: false
+    },
+    {
+      name: 'Scrabble',
+      id: 5,
+      selected: false
+    },
+    {
+      name: 'Monopoly',
+      id: 6,
+      selected: true
+    },
+    {
+      name: 'Uno',
+      id: 7,
+      selected: false
+    }
+  ];
 
   constructor(private router: Router, private readonly route: ActivatedRoute) {
     this.route.params.subscribe(params => console.log(params));
+    this.name = `Angular! v${VERSION.full}`;
+    this.getSelected();
   }
 
   ngOnInit(): void {
+  }
+  getSelected() {
+    this.selectedgames = this.games.filter(s => {
+      return s.selected;
+    });
+    this.selectedcount = this.selectedgames.length;
+  }
+
+  clearSelection() {
+    this.searchText = '';
+    this.games = this.games.filter(g => {
+      g.selected = false;
+      return true;
+    });
+    this.getSelected();
+  }
+
+  deleteGame(id: number) {
+    this.searchText = '';
+    this.games = this.games.filter(g => {
+      if (g.id === id) {
+        g.selected = false;
+      }
+
+      return true;
+    });
+    this.getSelected();
+  }
+
+  clearFilter() {
+    this.searchText = '';
   }
 }
